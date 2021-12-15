6793ba6881fda133f41596fd0153401266d603e6
Julián LB <loperabarrientos@gmail.com>
2021-12-15 11:55:50 -0500

    Create the Sign Up screen and support i18n

---
+++ src/screens/SignUp/i18n.ts
@@ -0,0 +1,11 @@
+import i18next from 'i18next';
+
+i18next.addResources('es', 'SignUp', {
+  firstName: 'Nombre',
+  lastName: 'Apellido',
+  email: 'Email',
+  password: 'Password',
+  passwordConfirmation: 'Confirmación de Password',
+  signUpButton: 'Sign Up',
+  loginButton: 'Login'
+});
