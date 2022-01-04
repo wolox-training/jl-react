0be98ecbf141ecb612bc487898a29c9948d77082
Julián LB <loperabarrientos@gmail.com>
2022-01-04 10:44:40 -0500

    Add onLoginButtonClick support

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -3,7 +3,7 @@ import i18next from 'i18next';
 import { useMutation } from 'react-query';
 import { useHistory } from 'react-router-dom';
 
-import SignUpForm from 'components/forms/SignUpForm';
+import SignUpForm from 'components/Forms/SignUpForm';
 import { User } from 'utils/types';
 import { signUp } from 'services/UsersService';
 import Loading from 'components/Spinner/components/loading';
@@ -24,6 +24,10 @@ function SignUp() {
   const onValid = (user: User) => {
     mutate(user);
   };
+  const onLoginButtonClick = () => {
+    history.push('/');
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -31,7 +35,7 @@ function SignUp() {
           <div className={styles.logo} />
           <SignUpForm onValid={onValid} />
           <div>
-            <button className="button-redirect" type="button">
+            <button className="button-redirect" type="button" onClick={onLoginButtonClick}>
               {i18next.t('SignUp:loginButton')}
             </button>
           </div>
