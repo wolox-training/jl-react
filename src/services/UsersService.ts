bd44da500ea78063186cb193150a9cffcc46e103
Julián LB <loperabarrientos@gmail.com>
2022-01-04 10:22:08 -0500

    Add the login endpoint service to users service.

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,4 +1,7 @@
 import api from 'config/api';
-import { User } from 'utils/types';
+import { User, UserCredentials } from 'utils/types';
 
 export const signUp = (userPayload: User) => api.post('/users', userPayload);
+
+export const login = (userCredentialsPayload: UserCredentials) =>
+  api.post('/users/sign_in', userCredentialsPayload);
