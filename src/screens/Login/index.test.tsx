12e18f5e0567ea19866ba20b05b99ff2e9b20243
Julián LB <loperabarrientos@gmail.com>
2022-01-04 12:10:14 -0500

    Test the Login component

---
+++ src/screens/Login/index.test.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { waitFor } from '@testing-library/react';
+import { mount } from 'enzyme';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
+
+import LoginForm from 'components/Forms/LoginForm';
+import { UserCredentials, User } from 'utils/types';
+import { VALID_USER } from 'constants/mock-users';
+
+import Login from '.';
+
+const server = setupServer(
+  rest.post<UserCredentials, User>(`${process.env.REACT_APP_API_BASE_URL}/users/sign_in`, (req, res, ctx) => {
+    const { email, password } = req.body;
+
+    if (email === VALID_USER.email && password === VALID_USER.password) {
+      return res(
+        ctx.set('access-token', 'xRmxsCcRsIoZKzpv7x6hog')
+      );
+    } else {
+      return res(
+        ctx.status(401)
+      );
+    }
+  })
+);
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush
+  })
+}));
+
+describe('Login', () => {
+  // eslint-disable-next-line init-declarations
+  let wrapper: any;
+  const queryClient = new QueryClient();
+
+  beforeAll(() => {
+    wrapper = mount(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={['/']}>
+          <Login />
+        </MemoryRouter>
+      </QueryClientProvider>
+    );
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('renders one <LoginForm /> component', () => {
+    expect(wrapper.find(LoginForm)).toHaveLength(1);
+  });
+
+  describe('when inputs are valid', () => {
+    it('should redirect to /home', async () => {
+      // GIVEN
+      wrapper
+        .find('.form-input')
+        .at(0)
+        .instance().value = VALID_USER.email;
+      wrapper
+        .find('.form-input')
+        .at(1)
+        .instance().value = VALID_USER.password;
+
+      // WHEN
+      wrapper
+        .find('.form-submit')
+        .hostNodes()
+        .simulate('submit');
+
+      // THEN
+      // eslint-disable-next-line max-nested-callbacks
+      await waitFor(() => expect(mockHistoryPush).toHaveBeenCalledWith('/home'));
+    });
+  });
+
+  describe('when click on signUpButton', () => {
+    it('should redirect to / path', async () => {
+      // WHEN
+      wrapper
+        .find('.button-redirect')
+        .hostNodes()
+        .simulate('click');
+
+      // THEN
+      // eslint-disable-next-line max-nested-callbacks
+      await waitFor(() => expect(mockHistoryPush).toHaveBeenCalledWith('/sign_up'));
+    });
+  });
+});
