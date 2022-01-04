ec546b2dd0b109b7244377f09b9d32e38d6f5586
Julián LB <loperabarrientos@gmail.com>
2022-01-03 20:30:03 -0500

    Add both enzyme and adpater utilities support for react 17

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,9 @@
 import '@testing-library/jest-dom';
 import 'mutationobserver-shim';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
 
 jest.mock('i18next', () => ({
   t: (key: string, params: Record<string, string>) => (params ? `${key} ${JSON.stringify(params)}` : key)
