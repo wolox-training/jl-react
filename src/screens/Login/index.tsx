d826ddec9aa4b523d12c09ca1eb71f620838d023
Julián LB <loperabarrientos@gmail.com>
2022-01-04 12:09:58 -0500

    Add react-router-dom support to Login component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import i18next from 'i18next';
 import { useMutation } from 'react-query';
+import { useHistory } from 'react-router-dom';
 
 import Loading from 'components/Spinner/components/loading';
 import LoginForm from 'components/Forms/LoginForm';
@@ -10,18 +11,23 @@ import { login } from 'services/UsersService';
 import styles from './styles.module.scss';
 
 function Login() {
+  const history = useHistory();
   const [errorMsg, setErrorMsg] = useState('');
   const { isLoading, isError, mutate } = useMutation((data: UserCredentials) => login(data), {
     onSuccess: res => {
       // eslint-disable-next-line no-console
-      console.log(res);
+      console.log('access-token: ', res);
+      history.push('/home');
     },
     onError: () => {
-      setErrorMsg(i18next.t('FormValidations:network'));
+      setErrorMsg(i18next.t('FormValidations:credentials'));
     }
   });
   const onValid = (userCredentials: UserCredentials) => {
     mutate(userCredentials);
+  };
+  const onSignUpButtonClick = () => {
+    history.push('/sign_up');
   };
 
   return (
@@ -31,11 +37,11 @@ function Login() {
           <div className={styles.logo} />
           <LoginForm onValid={onValid} />
           <div>
-            <button className="button-redirect" type="button">
+            <button className="button-redirect" type="button" onClick={onSignUpButtonClick}>
               {i18next.t('Login:signUpButton')}
             </button>
           </div>
-          {isError && <span className="form-alert">{errorMsg}</span>}
+          {isError && <span className="form-alert row center">{errorMsg}</span>}
           {isLoading && (
             <div className="row center full-width">
               <Loading name="circle" />
