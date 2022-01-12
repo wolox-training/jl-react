f059a2d5db9c1df1779980725d4008b33cca1713
Julián LB <loperabarrientos@gmail.com>
2022-01-12 14:03:31 -0500

    Add isLogged function to check if the token exisits

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,7 +1,10 @@
 import api from 'config/api';
 import { User, UserCredentials } from 'utils/types';
+import LocalStorageService from 'services/LocalStorageService';
 
 export const signUp = (userPayload: User) => api.post('/users', userPayload);
 
 export const login = (userCredentialsPayload: UserCredentials) =>
   api.post('/users/sign_in', userCredentialsPayload);
+
+export const isLogged = () => !!LocalStorageService.getValue('token');
