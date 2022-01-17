f6af319069367a0f2814d8e42b3f65c323d82c47
Julián LB <loperabarrientos@gmail.com>
2022-01-17 13:46:27 -0500

    Add home support for BookList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import i18next from 'i18next';
 
 import logoWoloxImg from 'assets/LogoWolox.png';
 import LocalStorageService from 'services/LocalStorageService';
+import api from 'config/api';
+import BookList from 'components/BookList';
 
 import styles from './style.module.scss';
 
@@ -11,18 +13,26 @@ function Home() {
   const history = useHistory();
   const onLogoutButtonClick = () => {
     LocalStorageService.removeValue('token');
+    LocalStorageService.removeValue('client');
+    LocalStorageService.removeValue('uid');
+    api.deleteHeader('access-token');
+    api.deleteHeader('client');
+    api.deleteHeader('uid');
     history.push('/');
   };
 
   return (
-    <nav className={`row space-around ${styles.navBar}`}>
-      <Link className={styles.logo} to="/home">
-        <img className={styles.logoImage} src={logoWoloxImg} alt={`${i18next.t('Home:logoAlt')}`} />
-      </Link>
-      <button className={styles.logOut} type="button" onClick={onLogoutButtonClick}>
-        {i18next.t('Home:logout')}
-      </button>
-    </nav>
+    <div>
+      <nav className={`row space-around ${styles.navBar}`}>
+        <Link className={styles.logo} to="/home">
+          <img className={styles.logoImage} src={logoWoloxImg} alt={`${i18next.t('Home:logoAlt')}`} />
+        </Link>
+        <button className={styles.logOut} type="button" onClick={onLogoutButtonClick}>
+          {i18next.t('Home:logout')}
+        </button>
+      </nav>
+      <BookList />
+    </div>
   );
 }
 
