a2cf08df531ff53e1f7d1fe63ff7099876c6a173
Julián LB <loperabarrientos@gmail.com>
2022-01-17 12:28:41 -0500

    Add book list realted types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,3 +11,24 @@ export type UserCredentials = {
   email: string;
   password: string;
 };
+
+export type Book = {
+  id: number;
+  author: string;
+  title: string;
+  imageUrl: string;
+  editor: string;
+  year: string;
+  genre: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type BookList = {
+  page: Book[];
+  count: number;
+  totalPages: number;
+  totalCount: number;
+  currentPage: number;
+  nextPage?: number;
+};
