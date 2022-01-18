fde7a3dad9665a1f1705b7fcf8c43d4555a1308c
Julián LB <loperabarrientos@gmail.com>
2022-01-17 20:59:36 -0500

    Create BookList component

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import i18next from 'i18next';
 
 import { listBooks } from 'services/BooksService';
 import Loading from 'components/Spinner/components/loading';
+import BookCard from 'components/BookCard';
 
 import styles from './styles.module.scss';
 
 function BookList() {
-  const { isLoading, data } = useQuery('listBooks', listBooks);
+  const { data, status } = useQuery('listBooks', listBooks);
+
+  if (status === 'loading') {
+    return (
+      <div className="row center full-width">
+        <Loading name="circle" />
+      </div>
+    );
+  }
 
   return (
     <div className="container py-5 px-3">
       <div className="wrapper">
         <div className={`row center middle wrap ${styles.bookList}`}>
-          {data?.data?.page &&
-            data.data?.page.map(book => (
-              <div className={`column m-1 p-2 ${styles.book}`} key={book.id}>
-                <div className={styles.bookCover}>
-                  <img
-                    className={styles.bookCoverImage}
-                    src={book.imageUrl}
-                    alt={`${i18next.t('Home:bookCoverAlt')}`}
-                  />
-                </div>
-                <h2 className={styles.bookTitle}>{book.title}</h2>
-                <h3 className={styles.bookAuthor}>{book.author}</h3>
-              </div>
-            ))}
+          {data?.data?.page?.map(book => (
+            <BookCard book={book} key={book.id} />
+          ))}
         </div>
       </div>
-      {isLoading && (
-        <div className="row center full-width">
-          <Loading name="circle" />
-        </div>
-      )}
     </div>
   );
 }
